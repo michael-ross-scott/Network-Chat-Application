4b49ac7d6e26891e2755b136a8ab9fcfa52acf95
Michael Scott <33922586+michael-ross-scott@users.noreply.github.com>
2018-03-20 01:29:47 +0200

    Added image sending and receiving capabilities

--- src/Client.java
+++ src/Client.java
@@ -1,15 +1,24 @@
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
- * @author Setup
+ * @author Michael Scott
  */
+
 public class Client
 {
     /// RECEIVE responses from the server
@@ -23,40 +32,66 @@ public class Client
     private final String host;
     
     private String message;
-    //used to terminate the program from other threads
+    // used to terminate the program from other threads
     boolean running;
+    int flag = 0;
+    boolean firstRunRead = false;
+    boolean firstRunWrite = false;
+    String incomingMessage;
+    
+    // restricted username list
+    private String [] restricted = new String[]
+    {"exit()", "Upload:", "Download:", "~~Download Incoming~~", "ListImages()", "ListUsers()"};
     
     public Client(int port,String host)
     {
         this.port = port;
         this.host = host;
+        message="";
     }
     
     public void process()
     {
-        try {
+        try 
+        {
             running = true;
             
             clientSocket = new Socket(host,port);
-            System.out.println("Client connected");
+            System.out.println("\nClient connected");
             connectStreams();
             
             messageWriter mw = new messageWriter();
             mw.start();
             
-             while(running)
-             {
-                String incomingMessage;
+            // Server listener
+            while(running)
+            {
+                
                 try 
                 {
-                    incomingMessage = input.readUTF();
-                    System.out.println(incomingMessage);
+                		// If the flag is set to 1, the client must receive the image sent courtesy of the server
+                    if(flag == 1)
+                    {
+                        receiveImage();
+                        flag = 0;
+                    }
+                    // Reads the incoming messages 
+                    else
+                    {
+                        incomingMessage = input.readUTF();
+                        System.out.println(incomingMessage);
+                        if(firstRunRead == false)
+                        {
+                            System.out.println("Your working directory is: "+getWorkingDirectory());
+                            firstRunRead = true;
+                        }
+                    }
                 } 
                 
                 catch (IOException ex) 
                 {
                     System.out.println("Server has closed the connection, Press enter to terminate client.");
-                    //terminates messageWriter
+                    // Terminates messageWriter
                     running = false;
                     disconnect();
                 }
@@ -68,35 +103,167 @@ public class Client
         }
     }
     
-    //Reads the message written in the terminal
+    // Gets the working directory and prints it out for uploading purposes
+    private String getWorkingDirectory()
+    {
+        File f = new File("test.txt");
+        String path = f.getAbsolutePath();
+        String absolutePath = path.substring(0,path.lastIndexOf("/")+1);
+        return absolutePath;
+    }
+    
+    // Reads the message written in the terminal
     private void readMessage()
     {
-        if(!clientSocket.isClosed()){
+        if(!clientSocket.isClosed())
+        {
+            
+            if(firstRunWrite == false)
+            {
+                Scanner sc = new Scanner(System.in);
+                message = sc.nextLine();
+                
+                while(reservedUsername(message))
+                {
+                    message = sc.nextLine();
+                    System.out.println("text is a reserved word ");
+                }
+                firstRunWrite = true;
+                return;
+            }
+            
             Scanner sc = new Scanner(System.in);
             message = sc.nextLine();
         }
     }
     
-    //Writes the message that user entered in, in readMessage
-    private boolean writeMessages()
+    // Checks if the client enters a valid username (can't be an empty string or restricted word)
+    private boolean reservedUsername(String message)
     {
-        if(!clientSocket.isClosed()){
-            try 
+        if(message.equals(""))
+        {
+            return true;
+        }
+        
+        // Checks if the username is a part of restricted key words
+        for(int i = 0; i < restricted.length; i++)
+        {
+            if(message.equals(restricted[i]))
             {
-                output.writeUTF(message);
-                output.flush();
                 return true;
-            } 
-            catch (IOException ex) 
-            {
-                System.out.println("Connection lost with server");
-                return false;
             }
         }
         return false;
     }
     
-    //disconnects client from the server
+    //Writes the message that user entered in, sends a file that the user selects or downloads a file
+    private boolean writeMessages()
+    {
+        if(!clientSocket.isClosed())
+        {
+            // Sends normal message
+            if(!message.startsWith("Upload") && !message.startsWith("Download"))
+            {
+               try 
+               {
+                    output.writeUTF(message);
+                    output.flush();
+                    return true;
+                } 
+                catch (IOException ex) 
+                {
+                    System.out.println("Connection lost with server");
+                    return false;
+                }
+            }
+            // Uploads a file
+            else if(message.startsWith("Upload"))
+            {
+                try
+                {
+                    String path = message.substring(message.indexOf(" ")+1, message.length());
+                    BufferedImage image = ImageIO.read(new File(path));
+                    
+                    try
+                    {
+                        ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+                        ImageIO.write(image, "jpg", byteOutput);
+                        
+                        byte[] size = ByteBuffer.allocate(4).putInt(byteOutput.size()).array();
+                        
+                        System.out.println("\nClient: Uploading...");
+                        
+                        output.writeUTF("~~~Download incoming~~~");
+                        output.write(size);
+                        output.write(byteOutput.toByteArray());
+                        output.flush();
+                        
+                        System.out.println("Server: Upload successful.\n");
+                        
+                        return true;
+                    }
+                    catch (IOException ex)
+                    {
+                        System.out.println("Client: Image too big");
+                    }
+                } 
+                catch (IOException ex)
+                {
+                    System.out.println("Client: Image cannot be found\n");
+                    return true;
+                }
+            }
+            // Downloads the image 
+            else
+            {
+                try
+                {
+                    output.writeUTF(message);
+                    flag = 1;
+                } 
+                catch (IOException ex)
+                {
+                    System.out.println("Connection lost with server");
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    // Receives the image from the server
+    public void receiveImage()
+    {
+        try
+        {
+            if(!incomingMessage.startsWith("Problem"))
+            {
+                byte[] sizeAr = new byte[4];
+                
+                input.read(sizeAr);
+                int size = ByteBuffer.wrap(sizeAr).asIntBuffer().get();
+
+                byte[] imageAr = new byte[size];
+                input.read(imageAr);
+                
+                ByteArrayInputStream bIn = new ByteArrayInputStream(imageAr);
+                BufferedImage image = ImageIO.read(bIn);
+                File f = new File("clientImg.jpg");
+                ImageIO.write(image, "jpg", f);
+            }
+            else
+            {
+                System.out.println("Problem saving image\n");
+            }
+        } 
+        catch (IOException ex)
+        {
+            System.out.println("Problem saving image.");
+        }
+        
+    }
+    
+    // Disconnects client from the server
     private void disconnect()
     {
         try 
@@ -111,7 +278,7 @@ public class Client
         }
     }
     
-    //connects input and output streams
+    // Connects input and output streams
     private void connectStreams()
     {
         try 
@@ -135,15 +302,18 @@ public class Client
         public void run()
         { 
             System.out.println("Enter your username to the server.");
+            
             while(running == true)
             {
                 readMessage();
+                
                 while(!message.equals("exit()"))
                 {
                     if(writeMessages() != true)
                     {
-                        break;
+                            break;
                     }
+                    
                     readMessage();
                 }
                 disconnect();
